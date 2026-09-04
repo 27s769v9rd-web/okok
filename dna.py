7865b7524d539e68aeb387b796da78b255448c26
27s769v9rd-web <27s769v9rd@privaterelay.appleid.com>
2026-09-03 22:39:02 -0500

    Update dna.py

--- dna.py
+++ dna.py
@@ -4,10 +4,25 @@ DNA
 
 
 def longest_subsequence(string_1, string_2):
-    """ADD YOUR CODE HERE """
-
-
-
+    string_1, string_2 = string_1.upper(), string_2.upper()
+    found = set()
+    max = 0
+    #iterate through all string1
+    for i in range(len(string_1)):
+        #iterate through all ending and only substrings with length greater than 2
+        for j in range(i+2, len(string_1) + 1):
+            substring = string_1[i:j]
+            #Check if the substring is also in string 2
+            if substring in string_2:
+                #if the substring is longest add to max and reset set
+                if len(substring) > max:
+                    max = len(substring)
+                    found = {sub}
+                #If we find another substring with same length add it to set
+                elif len(substring) == max:
+                    found.add(sub)
+    return sorted(list(found))
+    
 def main():
     """
     This main function reads the data input files and
